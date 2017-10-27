5ca9cf48e9807fe4ee2daa7e4aa1ee5e91af453e
Kojo Appiah <kojo.e.appiah@gmail.com>
2017-10-27 19:16:02 -0400

    Finished Compute 2439 Test

--- check_numerals.c
+++ check_numerals.c
@@ -83,6 +83,13 @@ START_TEST(test_compute_47)
 }
 END_TEST
 
+START_TEST(test_compute_2439)
+{
+    ck_assert_str_eq("MMCDXXXIX", IntegerToNumeral(2439));
+ 
+}
+END_TEST
+
 Suite * numeral_suite(void)
 {
     Suite *s;
@@ -104,7 +111,8 @@ Suite * numeral_suite(void)
     tcase_add_test(tc_core, test_compute_VII);
     tcase_add_test(tc_core, test_compute_XLIV);
     tcase_add_test(tc_core, test_compute_7);
-    tcase_add_test(tc_core, test_compute_47);    
+    tcase_add_test(tc_core, test_compute_47);
+    tcase_add_test(tc_core, test_compute_2439);
     suite_add_tcase(s, tc_core);
 
     return s;
